9286ce3ccb9d87bbb36f4377e74828b2755c051c
Ibara Takanashi <ibara1454@gmail.com>
2017-07-19 03:08:32 +0900

    Bug fixed

--- run_mpi_test.py
+++ run_mpi_test.py
@@ -1,1 +1,24 @@
-from mpi_test import test_pyss
+from mpi4py import MPI
+import pyss.mpi
+import pyss
+import numpy
+import scipy.io
+from pyss.util.contour import Circle
+import cProfile
+import pstats
+
+A = scipy.io.mmread("matrix/wathen100.mtx")
+B = scipy.sparse.eye(30401)
+option = {
+    'l': 10,
+    'm': 2,
+    'n': 12,
+    'refinement': {
+        'max_it': 1,
+        'tol': 1e-6
+    }
+}
+contour = Circle(center=1, radius=0.1)
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+ws, vs, info = pyss.mpi.solve(A, B, contour, option, comm)

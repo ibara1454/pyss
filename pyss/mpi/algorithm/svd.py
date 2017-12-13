fb056a194d641bcf56352ce0bc63f57b1315b740
Ibara <ibara1454@gmail.com>
2017-12-13 19:17:47 +0900

    Change API of MPI SVD. And use inv to get the inverse of l

--- pyss/mpi/algorithm/svd.py
+++ pyss/mpi/algorithm/svd.py
@@ -6,7 +6,7 @@ import scipy
 import scipy.linalg
 
 
-def svd(a, comm, full_matrices=True, compute_uv=True, overwrite_a=False):
+def svd(a, comm, iters=1, compute_uv=True, overwrite_a=False):
     """
     Singular value decomposition with separeted matrix on MPI parallelism.
 
@@ -17,9 +17,12 @@ def svd(a, comm, full_matrices=True, compute_uv=True, overwrite_a=False):
         colume-direction.
     comm : MPI_comm
         MPI communicator.
-    full_matrices : bool, optional
-        If True (default), U and Vh are of shape (M, M), (N, N). If False,
-        the shapes are (M, K) and (K, N), where K = min(M, N).
+    iters : int
+        Iteration numbers of downgrade procedure.
+        use `iter=1` while the condition number of `a` is small,
+        use `iter=2` or further while the conditino number is large.
+        Over 3 times, the accumulation of round-off error might cause the
+        result with a large amount of error.
     compute_uv : bool, optional
         Whether to compute also U and Vh in addition to s. Default is True.
     overwrite_a : bool, optional
@@ -36,10 +39,19 @@ def svd(a, comm, full_matrices=True, compute_uv=True, overwrite_a=False):
         Of shape (N, N) or (K, N) depending on full_matrices.
     For compute_uv=False, only s is returned.
     """
-    a, b = __dg_proc(a, comm)
+    a, b = __dg_proc_n(a, iters, comm)
     U, s, Vh = scipy.linalg.svd(b)
     U = a @ U
     return U, s, Vh
+
+
+def __dg_proc_n(a, n, comm):
+    row, col = a.shape
+    b = numpy.eye(col)
+    for _ in range(n):
+        a, b_n = __dg_proc(a, comm)
+        b = b_n @ b
+    return a, b
 
 
 def __dg_proc(a, comm):
@@ -80,8 +92,11 @@ def __dg_proc(a, comm):
     d[d <= 0] = eps
     d12 = numpy.diag(d / 2)
     d12i = __inv_diag(d12)
-    # TODO: use more effecient way to calculate d12 @ li instead of solve
-    b = scipy.linalg.solve(l.T, d).T
+    # TODO: use more effecient way to calculate d12 @ li instead of inv
+    # linalg.inv takes more time but have higher accuracy than lin.solve
+    b = d12 @ scipy.linalg.inv(l)
+    # The alternative, by using the equation `L^t X^t = D^t`
+    # b = scipy.linalg.solve(l.T, d12).T
     return a @ l @ d12i, b
 
 

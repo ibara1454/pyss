e6474d3168ae1b5473405c6f6964d0cb6590a166
Ibara <ibara1454@gmail.com>
2018-01-08 22:54:00 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='pyss',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages("pyss"),
     install_requires=[
         'numpy>=1.10.0,<2'
     ],
@@ -15,6 +15,7 @@ setup(
     extras_require={
         "mpi": ["mpi4py>=2.0.0,<3"]
     },
+    test_suite='test',
     description='Eigen solver Sakurai-Sugiura method',
     author='Ibara Takanashi',
     author_email='ibara1454@gmail.com',
